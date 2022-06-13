0bd01d0cbd8fca4db80355f1cbfcc809ca095da4
brenekv.03 <brenekv.03@A321-15>
2022-06-13 13:10:27 +0200

    Vysledek

---
+++ CiselneSoustavy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prevody
+{
+    class CiselneSoustavy
+    {
+        public string BinToDec(int bin)
+        {
+            double celek=0;
+            for(int i = 0;bin>0;i++)
+            {
+                int cislo = bin % 10;
+                if(cislo==1)
+                {
+                    celek += Math.Pow(2, i);
+                }
+                bin /= 10;
+            }
+            return celek.ToString();
+        }
+    }
+}
