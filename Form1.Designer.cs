c89c55543d7d04c809826c5afa866e1293ec917c
brenekv.03 <brenekv.03@A321-15>
2022-06-13 13:12:28 +0200

    tlaaaaaacitko

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(507, 181);
+            this.button1.Location = new System.Drawing.Point(-7, -1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(810, 458);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
